b24ea9c9a0aad4c5b2f118bf893ae8adad8d173b
Aryan6290 <aryanbarnwal007@gmail.com>
2021-01-02 23:15:24 +0530

    Adapter fixed

--- app/src/main/java/com/example/breakingbad/CharacterFragment.kt
+++ app/src/main/java/com/example/breakingbad/CharacterFragment.kt
@@ -1,54 +1,53 @@
 package com.example.breakingbad
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.Response
+import com.android.volley.Response.Listener
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.example.breakingbad.Adapter.CharacterAdapter
 import com.example.breakingbad.models.Character
+import com.example.breakingbad.utils.MySingleton
 import org.json.JSONArray
+import org.json.JSONObject
 
 class CharacterFragment : Fragment() {
-
+    var count: Int = 0
     lateinit var recyclerView: RecyclerView
+    private var mAdapter:CharacterAdapter?=null
+    private var characterList:ArrayList<Character>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private var url:String="https://www.breakingbadapi.com/api/characters"
 
-    }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view: View = inflater.inflate(R.layout.fragment_character, container, false)
-        var characterArray=ArrayList<Character>()
-        var count:Int=0
 
-        val url: String = "https://www.breakingbadapi.com/api/characters"
-
-
-        recyclerView = view.findViewById(R.id.character_recyclerview)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(activity)
-        val adapter=CharacterAdapter(characterArray)
-        recyclerView.adapter=adapter
-        Toast.makeText(context,"LMAOOOO + ${count}",Toast.LENGTH_SHORT).show()
-        return view
+        return inflater.inflate(R.layout.fragment_character, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    private fun getData(){
+        var jsonArrayRequest=JsonArrayRequest(Request.Method.GET,url,null, {
 
+
+        }, {
+
+        })
+    }
 }
 
-///     val character:Character= Character(characterObject.getString("name"),characterObject.getString("img"),characterObject.getString("potrayed"),characterObject.getString("birthday"),characterObject.getString("occupation"),characterObject.getString("nickname"),characterObject.getString("status"))
-//
-//characterArray.add(character)/
+

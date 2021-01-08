52307dfcf0d01e3aab1898a20e10c393d509f070
Aryan6290 <aryanbarnwal007@gmail.com>
2021-01-09 00:45:03 +0530

    Bug fixes

--- app/src/main/java/com/example/breakingbad/screens/HomeActivity.kt
+++ app/src/main/java/com/example/breakingbad/screens/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.example.breakingbad.screens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,6 +15,8 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val toolbar=findViewById<androidx.appcompat.widget.Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolbar)
 
         val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottom_nav)
         val navController=findNavController(R.id.fragment)

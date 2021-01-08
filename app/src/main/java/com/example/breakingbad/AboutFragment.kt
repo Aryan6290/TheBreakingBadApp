f3fdcbabe69f9c124c14f95f0a5c4334a735314e
Aryan6290 <aryanbarnwal007@gmail.com>
2021-01-08 23:12:05 +0530

    About Fragment Done , bug fixes

--- app/src/main/java/com/example/breakingbad/AboutFragment.kt
+++ app/src/main/java/com/example/breakingbad/AboutFragment.kt
@@ -1,10 +1,13 @@
 package com.example.breakingbad
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
 
 
@@ -24,7 +27,12 @@ class AboutFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_about, container, false)
-        Toast.makeText(context,"LMAOOOO",Toast.LENGTH_SHORT).show()
+        view.findViewById<ImageView>(R.id.github_link).setOnClickListener{
+            val url = "https://github.com/Aryan6290/TheBreakingBadApp"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
 
         return view
     }
